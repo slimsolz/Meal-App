adc46b62f18fdde6c3bc0a8f5b7c4dcc9f455e8f
Odumah Solomon <odumahs@gmail.com>
2018-05-23 21:12:59 +0100

    Add POST /auth/signin route and test for the route

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,6 +14,7 @@ router.get('/', (req, res, next) => {
 
 // User
 router.post('/auth/signup', Middleware.validateSignUp, UserController.signUp);
+router.post('/auth/signin', Middleware.validateSignIn, UserController.signIn);
 
 // 404 page
 router.get('*', (req, res) => {
